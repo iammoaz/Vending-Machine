5c202c51b06611b1531ca14016f46a5c2746ec9a
Muhammad Moaz <m-moaz@hotmail.com>
2017-01-02 23:25:22 +0500

    Initial Commit

---
+++ Vending Machine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vending Machine
+//
+//  Created by Muhammad Moaz on 1/2/17.
+//  Copyright © 2017 Muhammad Moaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
