b82785607747b1b11227d6b6466d436ee0fd0be1
Muhammad Moaz <m-moaz@hotmail.com>
2017-01-03 13:34:09 +0500

    Adds UICollectionView and UI Elements in storyboard

---
+++ Vending Machine/VendingItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  VendingItemCell.swift
+//  Vending Machine
+//
+//  Created by Muhammad Moaz on 1/3/17.
+//  Copyright © 2017 Muhammad Moaz. All rights reserved.
+//
+
+import UIKit
+
+class VendingItemCell: UICollectionViewCell {
+    @IBOutlet weak var iconView: UIImageView!
+    
+    static let reuseIdentifier = "VendingItemCell"
+}
--- Vending Machine/ViewController.swift
+++ Vending Machine/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
+fileprivate let screenWidth = UIScreen.main.bounds.width
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var balanceLabel: UILabel!
+    @IBOutlet weak var quantityLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setupCollectionViewCells()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: - Setup
+    func setupCollectionViewCells() {
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 20, right: 0)
+        
+        let padding: CGFloat = 10
+        let itemWidth = screenWidth/3 - padding
+        let itemHeight = screenWidth/3 - padding
+        
+        layout.itemSize = CGSize(width: itemWidth, height: itemHeight)
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        
+        collectionView.collectionViewLayout = layout
+    }
 
 }
 
