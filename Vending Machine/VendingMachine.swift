b2ea6101f73b54169bac23c750038ae69caa9a6e
Muhammad Moaz <m-moaz@hotmail.com>
2017-01-03 14:11:04 +0500

    Adds VendingMachine property lists

--- Vending Machine/VendingMachine.swift
+++ Vending Machine/VendingMachine.swift
@@ -26,3 +26,25 @@ protocol VendingMachine {
     func vend(_ quantity: Int, _ selection: VendingSelection) throws
     func deposit(_ amount: Double)
 }
+
+struct Item: VendingItem {
+    let price: Double
+    var quantity: Int
+}
+
+class FoodVendingMaching: VendingMachine {
+    let selection: [VendingSelection] = [.soda, .dietSoda, .chips, .cookie, .sandwich, .wrap, .candyBar, .popTart, .water, .fruitJuice, .sportsDrink, .gum]
+    
+    var inventory: [VendingSelection : VendingItem]
+    var amountDeposited: Double = 10.0
+    
+    required init(inventory: [VendingSelection : VendingItem]) {
+        self.inventory = inventory
+    }
+    
+    func vend(_ quantity: Int, _ selection: VendingSelection) throws {
+    }
+    
+    func deposit(_ amount: Double) {
+    }
+}
